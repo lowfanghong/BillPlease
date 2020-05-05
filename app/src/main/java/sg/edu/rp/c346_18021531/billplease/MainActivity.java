4bac4a66203949b89a093c94e826763356dee198
lowfanghong <18021531@myrp.edu.sg>
2020-05-05 17:11:55 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346_18021531/billplease/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346_18021531.billplease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
